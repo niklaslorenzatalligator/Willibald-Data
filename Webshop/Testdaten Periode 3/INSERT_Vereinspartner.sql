7f6a2b1544a7f9096f2ecefc31c164cc9b232d5d
niklaslorenzatalligator <niklas.lorenz@alligator-company.com>
2025-06-12 11:56:53 +0200

    Added new bi temp test data for period 3

--- Webshop/Testdaten Periode 3/INSERT_Vereinspartner.sql
+++ Webshop/Testdaten Periode 3/INSERT_Vereinspartner.sql
@@ -1,7 +1,5 @@
-INSERT INTO [VereinsPartner] ([VereinsPartnerID], [KundeIDVerein], [Rabatt1], [Rabatt2], [Rabatt3]) VALUES	 ('GlückAuf', '5', '2', '4', '8');
-INSERT INTO [VereinsPartner] ([VereinsPartnerID], [KundeIDVerein], [Rabatt1], [Rabatt2], [Rabatt3]) VALUES	 ('Morgenstern', '146', '3', '6', '12');
-INSERT INTO [VereinsPartner] ([VereinsPartnerID], [KundeIDVerein], [Rabatt1], [Rabatt2], [Rabatt3]) VALUES	 ('VolleRose', '218', '1', '2', '5');
-INSERT INTO [VereinsPartner] ([VereinsPartnerID], [KundeIDVerein], [Rabatt1], [Rabatt2], [Rabatt3]) VALUES	 ('WochenendGLück', '99', '5', '10', '18');
-INSERT INTO [VereinsPartner] ([VereinsPartnerID], [KundeIDVerein], [Rabatt1], [Rabatt2], [Rabatt3]) VALUES	 ('Druff1848', '42', '3', '6', '12');
-INSERT INTO [VereinsPartner] ([VereinsPartnerID], [KundeIDVerein], [Rabatt1], [Rabatt2], [Rabatt3]) VALUES	 ('Blaetterglueck', '84', '4', '8', '16');
-INSERT INTO [VereinsPartner] ([VereinsPartnerID], [KundeIDVerein], [Rabatt1], [Rabatt2], [Rabatt3]) VALUES	 ('Abendröte', '300', '2', '3', '6');
+INSERT INTO [VereinsPartner] ([CustomerID], [ExternalID], [CustomerStatus], [CustomerDiscount], [ValidFrom]) VALUES ('Ruhrmorig', '146', 'Gold', '2.0', '01.02.2025')
+INSERT INTO [VereinsPartner] ([CustomerID], [ExternalID], [CustomerStatus], [CustomerDiscount], [ValidFrom]) VALUES ('VolleRose', '218', 'Silber', '1.0', '20.01.2025')
+INSERT INTO [VereinsPartner] ([CustomerID], [ExternalID], [CustomerStatus], [CustomerDiscount], [ValidFrom]) VALUES ('Blaetterglueck', '84', 'Bronze', '0.0', '10.01.2025')
+INSERT INTO [VereinsPartner] ([CustomerID], [ExternalID], [CustomerStatus], [CustomerDiscount], [ValidFrom]) VALUES ('GlückAuf', '5', '', '', '01.02.2025')
+INSERT INTO [VereinsPartner] ([CustomerID], [ExternalID], [CustomerStatus], [CustomerDiscount], [ValidFrom]) VALUES ('WochenendGLück', '99', 'Gold', '2.0', '01.02.2025')
